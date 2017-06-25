bde4282153f924496104d98d1134773d01c785dd
lcgkiller <lcgkiller@gmail.com>
2017-06-25 22:12:40 +0900

    First Commmit

--- functional_tests.py
+++ functional_tests.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
 
-browser = webdriver.Firefox()
+browser = webdriver.Chrome()
 browser.get('http://localhost:8000')
 
-assert 'Django' in broswer.title
-
+assert 'Django' in browser.title
