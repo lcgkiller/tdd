4593572ae039b1a0fa36e1d7da833ec4f375f635
lcgkiller <lcgkiller@gmail.com>
2017-06-26 07:20:06 +0900

    첫 단위테스트와 url 맵핑 그리고 임시 뷰

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,3 +1,4 @@
+from django.http import HttpRequest
 from django.test import TestCase
 
 # Create your tests here.
@@ -11,3 +12,13 @@ class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest() # HttpRequest 객체 생성
+        response = home_page(request) # home_page 뷰에 위 객체를 전달해 응답(response)을 얻는다.
+
+        self.assertTrue(response.content.startswith(b'<html>')) # 응답내용이 html로 시작해서 html로 끝나는지 확인
+        self.assertIn(b'<title>To-Do lists</tittle>', response.content) # 타이틀 태그에 To-Do lists라는 단어가 있는지 확인
+        self.assertTrue(response.content.endswith(b'</html>'))
+
