d7088263b36de4afa6ece856ac5cebc835b91b86
lcgkiller <lcgkiller@gmail.com>
2017-06-27 22:21:11 +0900

    70p 오류 및 기타 오타 수정

--- functional_tests.py
+++ functional_tests.py
@@ -16,7 +16,7 @@ class NewVisitorTest(unittest.TestCase):
     def check_for_row_in_list_table(self, row_text):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.txt for row in rows])
+        self.assertIn(row_text, [row.text for row in rows])
 
     # test로 시작하는 메소드는 테스트 메소드이며, 테스트 실행자에 의해서 실행된다.
     def test_can_start_a_list_and_retrieve_it_later(self):
@@ -33,11 +33,9 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
         # 페이지를 다시 갱신되고, 두 개의 아이템이 목록에 보인다.
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-
-        self.check_for_row_in_list_table('2: 공작깃털을 이용해서 그물만들기')
         self.check_for_row_in_list_table('1: 공작깃털 사기')
+        self.check_for_row_in_list_table('2: 공작깃털을 이용해서 그물만들기')
+
         self.fail('Finish the test!')
 
 
