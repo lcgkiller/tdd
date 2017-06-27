dee47902d2186eb90c2e077e47a4daa5b860f891
lcgkiller <lcgkiller@gmail.com>
2017-06-27 22:18:01 +0900

    functional_test 헬퍼메소드(check_for_row_in_list_table) 작성

--- functional_tests.py
+++ functional_tests.py
@@ -12,6 +12,12 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    # 헬퍼메소드
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.txt for row in rows])
+
     # test로 시작하는 메소드는 테스트 메소드이며, 테스트 실행자에 의해서 실행된다.
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
@@ -29,11 +35,9 @@ class NewVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: 공작깃털 사기', [row.text for row in rows])
-        self.assertIn(
-            '2: 공작깃털을 이용해서 그물 만들기', [row.text for row in rows]
-        )
 
+        self.check_for_row_in_list_table('2: 공작깃털을 이용해서 그물만들기')
+        self.check_for_row_in_list_table('1: 공작깃털 사기')
         self.fail('Finish the test!')
 
 
