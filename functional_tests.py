b191d90ff2f1ec67a0dabda73d4dfca4c31ee288
lcgkiller <lcgkiller@gmail.com>
2017-06-27 22:14:32 +0900

    functional_test.py, home.html, tests.py, views.py 변경내용 표시

--- functional_tests.py
+++ functional_tests.py
@@ -7,7 +7,7 @@ import unittest
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Chrome()
-        self.browser.implicitly_wait(3)
+        self.browser.implicitly_wait(10)
 
     def tearDown(self):
         self.browser.quit()
@@ -16,30 +16,25 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
 
+        # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
 
+        # She is invited to enter a to-do item straight away
         inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertEqual(
-            inputbox.get_attribute('placeholder'),
-            'Enter a to-do item'
-        )
-
-        inputbox.send_keys('Buy peacock feathers')
-
-        # 엔터키를 치면 페이지가 갱신되면서 작업 목록에 "1: 공작깃털 사기" 아이템이 추가된다.
-        # Keys 클래스는 Enter나 Ctrl같은 특수 키 입력을 전송하는 역할을 한다.
+        inputbox.send_keys("공작깃털을 이용해서 그물만들기")
         inputbox.send_keys(Keys.ENTER)
+        # 페이지를 다시 갱신되고, 두 개의 아이템이 목록에 보인다.
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            "New to-do item did not appear in table"
+        self.assertIn('1: 공작깃털 사기', [row.text for row in rows])
+        self.assertIn(
+            '2: 공작깃털을 이용해서 그물 만들기', [row.text for row in rows]
         )
 
-        self.fail("Finish the test!")
+        self.fail('Finish the test!')
 
 
 if __name__ == '__main__':
--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,3 +1,4 @@
+import re
 from django.http import HttpRequest
 from django.template.loader import render_to_string
 from django.test import TestCase
@@ -9,12 +10,36 @@ from lists.views import home_page
 
 
 class HomePageTest(TestCase):
+    pattern_input_csrf = re.compile(r'<input[^>]*csrfmiddlewaretoken[^>]*>')
+
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
 
     def test_home_page_returns_correct_html(self):
-        request = HttpRequest()  # HttpRequest 객체 생성
-        response = home_page(request)  # home_page 뷰에 위 객체를 전달해 응답(response)을 얻는다.
+        request = HttpRequest()
+        response = home_page(request)
         expected_html = render_to_string('home.html')
-        self.assertEqual(response.content.decode(), expected_html)
+        self.assertEqual(
+            re.sub(self.pattern_input_csrf, '', response.content.decode()),
+            re.sub(self.pattern_input_csrf, '', expected_html)
+        )
+
+    def test_home_page_can_save_a_POST_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = '신규 작업 아이템'
+
+        response = home_page(request)
+
+        self.assertIn('신규 작업 아이템', response.content.decode())
+        expected_html = render_to_string(
+            'home.html',
+            {
+                'new_item_text': '신규 작업 아이템'
+            }
+        )
+        self.assertEqual(
+            re.sub(self.pattern_input_csrf, '', response.content.decode()),
+            re.sub(self.pattern_input_csrf, '', expected_html)
+        )
